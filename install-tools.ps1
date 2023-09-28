94add1605fd543fa848432ae87eeb990fa42f89f
Sam Armstrong <saarmstr@microsoft.com>
2023-09-28 18:38:26 -0400

    Add zig installer

--- install-tools.ps1
+++ install-tools.ps1
@@ -1,3 +1,5 @@
 choco install neovim fzf ripgrep make lazygit fd -y
-
+irm get.scoop.sh -outfile 'install.ps1'
+.\install.ps1 -RunAsAdmin  "$HOME\.scoop"
+scoop install zig 
 New-Item -ItemType SymbolicLink -Path $HOME/AppData/Local/nvim -Target $PSScriptRoot/nvim
