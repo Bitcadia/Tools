786736ee05533c18fc55c8f49263bae08ba168f0
Sam Armstrong <saarmstr@microsoft.com>
2023-10-01 14:23:18 -0400

    Fix pwsh

--- install-tools.ps1
+++ install-tools.ps1
@@ -1,5 +1,42 @@
-choco install neovim fzf ripgrep make lazygit fd -y
 irm get.scoop.sh -outfile 'install.ps1'
 .\install.ps1 -RunAsAdmin  "$HOME\.scoop"
-scoop install zig 
+[System.Environment]::SetEnvironmentVariable("Path",$env:Path+";$HOME\.scoop\shims")
+$StartMarker = "# DEF from install-tools.sh"
+$EndMarker = "# ENDEF from install-tools.sh"
+
+if ((Get-Content $PROFILE) -contains $StartMarker -and (Get-Content $PROFILE) -contains $EndMarker) {
+    $content = Get-Content $PROFILE 
+    $inRange = $false
+    $newContent = @()
+
+    foreach ($line in $content) {
+        if ($line -eq $EndMarker) {
+            $inRange = $false
+        }
+        if (-not $inRange) {
+            $newContent += $line
+        }
+        if ($line -eq $StartMarker) {
+            $inRange = $true
+        }
+    }
+
+    Set-Content -Path $PROFILE -Value $newContent
+    Write-Host "Lines between '$StartMarker' and '$EndMarker' have been removed from $PROFILE."
+} else {
+    Write-Host "Could not find the start and end markers in $PROFILE. No changes made."
+}
+Add-Content -Path $PROFILE -Value "$StartMarker
+[System.Environment]::SetEnvironmentVariable(`"Path`",`$env:Path+`";`$HOME\.scoop\shims`")
+$EndMarker"
+
+scoop bucket add extras
+scoop install zig
+scoop install neovim
+scoop install fzf
+scoop install ripgrep
+scoop install make
+scoop install lazygit
+scoop install fd
+
 New-Item -ItemType SymbolicLink -Path $HOME/AppData/Local/nvim -Target $PSScriptRoot/nvim
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,3 @@
 return {
   "telescope.nvim",
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
-  },
 }
