99a62a83fa331ccb76ac2ec0163208d43544fe71
Sam Armstrong <saarmstr@microsoft.com>
2024-02-21 18:48:00 -0500

    Add sed to windows

--- install-tools.ps1
+++ install-tools.ps1
@@ -38,5 +38,6 @@ scoop install ripgrep
 scoop install make
 scoop install lazygit
 scoop install fd
+scoop install sed
 
 New-Item -ItemType SymbolicLink -Path $HOME/AppData/Local/nvim -Target $PSScriptRoot/nvim
